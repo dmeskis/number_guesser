7973816af801d7905d66b00cd0323bcefbd12d1f
Mesky <dmeskis@gmail.com>
2018-11-14 10:28:35 -0700

    User can guess and it displays users guess

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,11 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+function generateRandomNumber() {
+  Math.floor(Math.random() * 100);
+}
+var randomNumber = generateRandomNumber()
+
+function guess() {
+  var userGuess = document.getElementById('user-input').value
+  document.getElementById('user-guess').innerHTML = parseInt(userGuess);
+}
