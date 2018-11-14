5a510e55bb1c3b0f7e0ac85ef92f3b3fb02d1844
Mesky <dmeskis@gmail.com>
2018-11-14 10:37:15 -0700

    Working on if else statements for user guess

--- javascript/index.js
+++ javascript/index.js
@@ -3,9 +3,17 @@ document.getElementById('date').innerHTML = new Date().toDateString();
 function generateRandomNumber() {
   Math.floor(Math.random() * 100);
 }
-var randomNumber = generateRandomNumber()
+var randomNumber = parseInt(generateRandomNumber())
 
 function guess() {
-  var userGuess = document.getElementById('user-input').value
-  document.getElementById('user-guess').innerHTML = parseInt(userGuess);
+  var userGuess = parseInt(document.getElementById('user-input').value)
+  document.getElementById('result-top-text').innerHTML = "Your last guess was"
+  document.getElementById('user-guess').innerHTML = userGuess;
+  if (userGuess > randomNumber) {
+    document.getElementById('result-bottom-text').innerHTML = "That is too low"
+  } else if (userGuess < randomNumber) {
+    document.getElementById('result-bottom-text').innerHTML = "That is too high"
+  } else if (userGuess === randomNumber) {
+    document.getElementById('result-bottom-text').innerHTML = "BOOM!"
+  }
 }
