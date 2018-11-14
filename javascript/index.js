4cb3813d4099c9e28a62d40b3edc7790ee9f6d1c
Mesky <dmeskis@gmail.com>
2018-11-14 10:45:53 -0700

    Guessing functionality works

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,19 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
 
 function generateRandomNumber() {
-  Math.floor(Math.random() * 100);
+  return Math.floor(Math.random() * 100);
 }
-var randomNumber = parseInt(generateRandomNumber())
+
+var randomNumber
+randomNumber = generateRandomNumber()
 
 function guess() {
   var userGuess = parseInt(document.getElementById('user-input').value)
   document.getElementById('result-top-text').innerHTML = "Your last guess was"
   document.getElementById('user-guess').innerHTML = userGuess;
   if (userGuess > randomNumber) {
-    document.getElementById('result-bottom-text').innerHTML = "That is too low"
-  } else if (userGuess < randomNumber) {
     document.getElementById('result-bottom-text').innerHTML = "That is too high"
+  } else if (userGuess < randomNumber) {
+    document.getElementById('result-bottom-text').innerHTML = "That is too low"
   } else if (userGuess === randomNumber) {
     document.getElementById('result-bottom-text').innerHTML = "BOOM!"
   }
