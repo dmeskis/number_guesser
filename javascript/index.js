53ec05d9bc003f0766c0a584df64873539a0a179
Mesky <dmeskis@gmail.com>
2018-11-14 13:11:36 -0700

    Now to fix clear function

--- javascript/index.js
+++ javascript/index.js
@@ -8,14 +8,28 @@ randomNumber = generateRandomNumber();
 
 function guess() {
   var userGuess = parseInt(document.getElementById('user-input').value)
-  document.getElementById('result-top-text').innerHTML = "Your last guess was"
-  document.getElementById('user-guess').innerHTML = userGuess;
-  if (userGuess > randomNumber) {
-    document.getElementById('result-bottom-text').innerHTML = "That is too high"
-  } else if (userGuess < randomNumber) {
-    document.getElementById('result-bottom-text').innerHTML = "That is too low"
-  } else if (userGuess === randomNumber) {
-    document.getElementById('result-bottom-text').innerHTML = "BOOM!"
+  var validGuess
+  if (userGuess > 100 || userGuess < 0) {
+    document.getElementById('user-input').value = '';
+    validGuess = false
+    alert("Please enter a number between 0 and 100");
+  } else {
+    validGuess = true
+    document.getElementById('result-top-text').innerHTML = "Your last guess was"
+    document.getElementById('user-guess').innerHTML = userGuess;
+  }
+
+  if (validGuess === true) {
+    if (userGuess > randomNumber) {
+      document.getElementById('result-bottom-text').innerHTML = "That is too high"
+    } else if (userGuess < randomNumber) {
+      document.getElementById('result-bottom-text').innerHTML = "That is too low"
+    } else if (userGuess === randomNumber) {
+      document.getElementById('result-bottom-text').innerHTML = "BOOM!"
+    }
+  }
+  else {
+    return;
   }
 }
 
@@ -23,12 +37,12 @@ function clear() {
   document.getElementById('user-input').value = '';
 }
 
-function handleChange(input) {
-    if (input.value < 0) {
-      input.value = 0;
-      alert("Please enter a number between 0 and 100");
-    } else if (input.value > 100) {
-      input.value = 100;
-      alert("Please enter a number between 0 and 100");
-    }
-}
+// function handleChange(input) {
+//     if (input.value < 0) {
+//       input.value = 0;
+//       alert("Please enter a number between 0 and 100");
+//     } else if (input.value > 100) {
+//       input.value = 100;
+//       alert("Please enter a number between 0 and 100");
+//     }
+// }
